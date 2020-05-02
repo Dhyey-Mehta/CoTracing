d1f8bce427119b0ff442fabc7526c1037ff53a16
varGeneric <16168572+varGeneric@users.noreply.github.com>
2020-05-02 16:01:46 -0400

    more stuff

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
-function loadJSON(files){
+async function loadJSON(files){
   if (files.length <= 0) {
     return false;
   }
-
-  let fr = new FileReader();
   
-  fr.onload = function(e) { 
-    var result = JSON.parse(e.target.result);
-    var formatted = JSON.stringify(result, null, 2);
-    document.getElementById('result').innerHTML = formatted;
-    console.log(result);
-  }
+  console.debug("Attempting to load "+files[0].name);
+  domstr = await ReadFilePr(files.item(0));
   
-  console.log("Attempting to load "+files[0].name);
-  fr.readAsText(files.item(0));
+  var result = JSON.parse(domstr);
+  //---- FOR TESTING ----
+  //var formatted = JSON.stringify(result, null, 2);
+  //document.getElementById('result').innerHTML = formatted;
+  console.debug(result);
+  //-- END FOR TESTING --
+  return result;
 }
 
-function compute(JSONToParse){
+function ReadFilePr(file){
+  return new Promise((resolve, reject) => {
+    var fr = new FileReader();  
+    fr.onload = () => {
+      resolve(fr.result)
+    };
+    fr.readAsText(file);
+  });
+}
+
+
+async function compute(JSONPromise){
     let placeVisit = [];
-    for (i in JSONToParse.timelineObjects){
-      if (JSONToParse.timelineObjects[i].hasOwnProperty('placeVisit')){
-          let latitude = JSONToParse.timelineObjects[i].placeVisit.location.latitudeE7;
-          let longitude = JSONToParse.timelineObjects[i].placeVisit.location.longitudeE7;
-          let startTime = JSONToParse.timelineObjects[i].placeVisit.duration.startTimestampMs;
-          let endTime = JSONToParse.timelineObjects[i].placeVisit.duration.endTimestampMs;
+    let locationData = await JSONPromise;
+    for (i in locationData.timelineObjects){
+      if (locationData.timelineObjects[i].hasOwnProperty('placeVisit')){
+          let latitude = locationData.timelineObjects[i].placeVisit.location.latitudeE7;
+          let longitude = locationData.timelineObjects[i].placeVisit.location.longitudeE7;
+          let startTime = locationData.timelineObjects[i].placeVisit.duration.startTimestampMs;
+          let endTime = locationData.timelineObjects[i].placeVisit.duration.endTimestampMs;
 
           placeVisit.push({latitude:latitude,longitude:longitude,startTime:startTime,endTime:endTime});
       }
     }
+    console.debug(placeVisit);
+    document.getElementById('result').innerHTML = JSON.stringify(placeVisit);
+    
+    return placeVisit;
 }
 
-/*
+
+let placeVisit = compute();
 let patient = [{latitude:438250504, longitude:-793731613, startTime:1585789139637, endTime:1586380498044}];
 let commonLocations = []
 
@@ -38,13 +54,14 @@ for(i in patient)
 {
   for(j in placeVisit)
   {
-    const bounds = 3000
-    let latLowerBound = Math.abs(i.latitude) - bounds;
-    let latUpperBound = Math.abs(i.latitude) + bounds;
-    let longLowerBound = Math.abs(i.longitude) - bounds;
-    let longUpperBound = Math.abs(i.longitude) + bounds;
-    if(j.latitude)
+    const bounds = 3000;
+    const quarintineTime = 86400000;
+    if((Math.abs(i.latitude - j.latitude) <= bounds) && (Math.abs(i.longitude - j.longitude) <= bounds)){
+      if(Math.abs(i.endTime-j.startTime)<8640000)
+      {
+        commonLocations[commonLocations.length] = {latitude: j.latitude, longitude: j.longitude, time: j.startTime}
+      }
+      
+    }
   }
 }
-
-*/
